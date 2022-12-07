b382c6c39b2536cf6fae40e1e16c287256db9e6b
Laiba Rashid <107211968+yomi-kii@users.noreply.github.com>
2022-12-07 11:14:13 +0000

    monster update function made

--- Monster.py
+++ Monster.py
@@ -1,29 +1,25 @@
 import random
 
+
 class Monster():
 
-  def __init__(self, name, hp, attack, abilities):
+  def __init__(self, name, fp, attack, abilities, ultimate):
     self.name = name
-    self.hp = hp
+    self.fp = fp
     self.attack = attack
-    self.abllities = abilities
+    self.ablities = abilities
     
     
 
 
   
   def update(self, player):
-    
-    pass
-
-
-
+    ability_number = random.randint(0,3)
+    ability_use = self.abilities[ability_number]
+    player.hp -= self.abilities[ability_use]
 
 
 
 class SmolBean(Monster):
   def __init__(self):
-    Monster.__init__(self, "SmolBean", 40, random.randint(5,10), ["Mushy", "Sugar snap", "Vine wrap", "Legumes in a pod"])
-
-
-    
+    Monster.__init__(self, "SmolBean", 40, random.randint(5,10), {"Mushy": 10, "Sugar snap": 7, "Vine wrap": 11}, {"Legumes in a pod" : 17})
